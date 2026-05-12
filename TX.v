6861527bfed202f4c79b8eac4dbc9f062a70a0bf
Lohith K S <lohithsubraya@feserver.mirafra.com>
2026-05-12 17:00:46 +0530

    UART Transmitter

---
+++ TX.v
@@ -0,0 +1,61 @@
+`timescale 1ns / 1ps
+
+module TX #(parameter WORD =8)(
+    input baud_clk,xmitH,sys_rst_l,
+    input [WORD -1 :0]xmit_dataH,// paralell input
+    output reg uart_REC_dataH,xmit_active,xmit_doneH
+    );
+   
+    reg [31:0]count;
+    // counter Logic	
+    always@(posedge baud_clk)begin 
+      if(xmitH == 1)
+            count = 1;
+      else if(count > 0)
+        	count = count +1;
+        else 
+            count = 0;    
+    end        
+    integer i;
+    // Tx operation
+    always@(posedge baud_clk or posedge sys_rst_l) begin
+        if(sys_rst_l) begin
+            count = 0;
+            uart_REC_dataH <=0;
+        end    
+        else begin
+            
+          if(count < 16) begin
+                    uart_REC_dataH <=0;
+                    i = WORD-1;
+                end
+                    
+          else if(count >=16 && count < 16*WORD+16) begin
+                    if((count %16) == 0)  begin
+                          uart_REC_dataH <= xmit_dataH[i];
+                          i= i-1;
+                    end
+                    else  uart_REC_dataH <= uart_REC_dataH;
+                    
+                end
+                
+          else if(count >=16*WORD)
+                        uart_REC_dataH <= 1;
+         end   
+      //Tx state output logic
+      if(count >=1 && count <160) begin
+        	xmit_active <=1;
+        	xmit_doneH <= 0;
+      end
+      else if(count >= 160) begin
+          	xmit_active <= 0;
+        	xmit_doneH <= 1;
+      end
+      else begin
+        xmit_active <=0;
+        xmit_doneH <= 0;
+      end
+          
+     end                                                
+endmodule
+
