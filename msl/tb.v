89fe3727f65c02f7a00c89b14e34e84e51674fc3
Lohith1801 <155318567+Lohith1801@users.noreply.github.com>
2026-05-19 23:38:44 +0530

    Update tb.v

--- msl/tb.v
+++ msl/tb.v
@@ -1,22 +1,51 @@
-module test;
+`timescale 1ns / 1ps
 
-//TX driver
- task drive_tx(input xmitH_in, input [WORD-1 : 0]xmit_dataH_in,output reg xmitH, output reg [WORD-1 : 0]xmit_dataH);
-   begin
-	@(negedge baud_clk);
-	xmitH <= xmitH_in;
-	xmit_dataH <= xmit_dataH_in;
-   end
- endtask
+module uart_test;
 
-//RX driver
- task drive_rx(input uart_REC_dataH_in, output reg uart_REC_dataH);
-  begin
-	@(negedge baud_clk);
-	uart_REC_dataH <= uart_REC_dataH_in;
-  end
- endtask
+parameter width = 8;
+parameter freq  = 50000000;
+parameter baudr = 9600;
+
+//global signals	
+reg sys_clk;
+reg sys_rst;
+
+//	
+reg xmit_h;
+reg [width-1:0] xmit_data_h;
+reg uart_rec_data_h;
+wire uart_clk;
+wire uart_xmit_data_h;
+
+wire xmit_done_h;
+
+wire [width-1:0] rec_data_h;
+
+wire rec_ready;
+
+wire rec_busy;
+
+wire xmit_active;
+
+uart #(
+    .freq(freq),
+    .baudr(baudr),
+    .width(width)
+) DUT (
+    .sys_clk(sys_clk),
+    .sys_rst(sys_rst),
+    .xmit_h(xmit_h),
+    .xmit_data_h(xmit_data_h),
+    .uart_rec_data_h(uart_rec_data_h),
+    .uart_clk(uart_clk),
+    .uart_xmit_data_h(uart_xmit_data_h),
+    .xmit_done_h(xmit_done_h),
+    .rec_data_h(rec_data_h),
+    .rec_ready(rec_ready),
+    .rec_busy(rec_busy),
+    .xmit_active(xmit_active)
+);
 
 
 
-	  
+endmodule
